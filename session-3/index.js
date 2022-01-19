9c9c68c1955011bbdbc9422d7844a002673b7321
tanatswak <t.kazangarare@outlook.com>
2022-01-19 19:55:05 +0000

    Task 2 function conversation

--- session-3/index.js
+++ session-3/index.js
@@ -3,3 +3,15 @@ const b = 10;
 let c = a + b;
 
 console.log(c);
+function sayHey() { 
+    console.log("Hey!"); 
+}
+
+sayHey();
+function conversation() {
+    sayHey();
+    console.log("How are you?");
+    console.log('Goodbye');
+    
+}
+conversation()
