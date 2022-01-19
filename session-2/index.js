4aa182dd214b19fe70cb3bb8068cbc502f3df3c7
tanatswak <t.kazangarare@outlook.com>
2022-01-19 18:38:22 +0000

    exisiting file

--- session-2/index.js
+++ session-2/index.js
@@ -1,2 +1,1 @@
-// This is a comment in JavaScript
-// You can see where this file is connected to your HTML in the index.html on line 14
+console.log("Hello world", 5+2+10)
--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,5 @@
-// This is a comment in JavaScript
-// You can see where this file is connected to your HTML in the index.html on line 14
+const a = 5;
+const b = 10;
+let c = a + b;
+
+console.log(c);
