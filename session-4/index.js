2057fa010f06c81523b7353860dd0126451aa6d2
tanatswak <t.kazangarare@outlook.com>
2022-01-24 20:03:51 +0000

    Task 2

--- session-4/index.js
+++ session-4/index.js
@@ -1,70 +1,59 @@
-// ****************************************************TASK 1 - OBJECTS****************************************************
+// ********************************************3********TASK 1 - OBJECTS****************************************************
+let personA = {
+  name: "Tanatswa",
+  age: "19",
+  location: "london",
+  likes: "pineapple",
+};
+let personB = {
+    name: "Tinashe",
+    age: "43",
+    location: "leeds",
+    likes: "travelling",
+  };
+  let personC = {
+    name: "Terry",
+    age: "25",
+    location: "reading",
+    likes: "running",
+  };
+  let personD = {
+    name: "Tanya",
+    age: "11",
+    location: "essex",
+    likes: "spiderman",
+  };
+function biography(person) {
+  return " Hi, my name is " + person.name + " . I am " + person.age + " years old, " +" I live in " + person.location + " and I like " + person.likes;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(biography(personA));
+console.log(biography(personB));
+console.log(biography(personC));
+console.log(biography(personD));
 
 
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
 
-// const x = 6;
-// const y = 4;
+const x = 14;
+const y = 8;
 
-// const addition = x + y;
+const addition = x + y;
 
-// console.log("Addition: x + y " + addition);
+console.log("Addition: x + y " + addition);
 
-// const subtraction = x - y;
+const subtraction = x - y;
 
-// console.log("Subtraction: x - y " + subtraction);
+console.log("Subtraction: x - y " + subtraction);
 
-// const multiplication = x * y;
+const multiplication = x * y;
 
-// console.log("multiplication: x * y " + multiplication);
+console.log("multiplication: x * y " + multiplication);
 
-// const division = x * y;
+const division = x * y;
 
-// console.log("Division: x + y " + division);
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log("Division: x + y " + division);
 
 
 // ****************************************************TASK 3****************************************************
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
